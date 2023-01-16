d90612a19e13ab91b2714a0b54ba5645e415d93d
HrechykhaYurii <grechikha.yura@gmail.com>
2023-01-16 16:12:53 +0200

    add task1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+
+startBtnEl.addEventListener('click', onClickStart);
+stopBtnEl.addEventListener('click', onClickStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+const PROMPT_DELAY = 1000;
+
+function onClickStart() {
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+  timerId = setInterval(changeColor, PROMPT_DELAY);
+}
+
+function onClickStop() {
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+  clearInterval(timerId);
+}
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,71 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+
+const startBtnEl = document.querySelector('[data-start]');
+const input = document.querySelector('#datetime-picker');
+const leftDays = document.querySelector('[data-days]');
+const leftHours = document.querySelector('[data-hours]');
+const leftMinutes = document.querySelector('[data-minutes]');
+const leftSeconds = document.querySelector('[data-seconds]');
+
+startBtnEl.addEventListener('click', onStartClick);
+
+startBtnEl.disabled = true;
+
+const PROMPT_DELAY = 1000;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startBtnEl.disabled = true;
+    }
+      startBtnEl.disabled = false;
+  },
+};
+
+flatpickr(input, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function onStartClick() {
+  let  timerId = setInterval(() => {
+    const selectedTime =  new Date(input.value);
+    const timeValue = selectedTime - new Date();
+
+    startBtnEl.disabled = true;
+
+    if (timeValue >= 0) {
+      let leftTime = convertMs(timeValue);
+      leftDays.textContent = leftTime.days;
+      leftHours.textContent = leftTime.hours;
+      leftMinutes.textContent = leftTime.minutes;
+      leftSeconds.textContent = leftTime.seconds;
+      } else {
+        Notiflix.Notify.success('Finish');
+        clearInterval(timerId);
+        }
+  },PROMPT_DELAY);
+}
